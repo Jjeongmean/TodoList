e7fe7e1349cd9d02e41c12d0d698dac60299c653
Jjeongmean <exo1461@naver.com>
2024-05-17 16:50:33 +0900

    20240517

--- src/main/java/com/todolist/controller/TodoListController.java
+++ src/main/java/com/todolist/controller/TodoListController.java
@@ -14,10 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.Optional;
@@ -116,11 +113,12 @@ public class TodoListController {
             model.addAttribute("todoFormDto", getTodoFormDto);
             return "todolist/rewrite";
         }
-        return "main";
+        return "redirect:/";
     }
 
 
     //삭제하기
+    @DeleteMapping("/todolist/{todoListId}/delete")
     public @ResponseBody ResponseEntity deleteTodo(@PathVariable("todoListId") Long todoListId,
                                                    Principal principal) {
         todoService.deleteList(todoListId);
